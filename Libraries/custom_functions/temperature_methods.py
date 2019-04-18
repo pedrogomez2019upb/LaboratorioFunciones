9c5199c67dfb29fd2d0034b2fba9d873eb231d8f
Pedro Gómez <pedro.gomez.2019@upb.edu.co>
2019-04-17 20:23:07 -0500

    Se añade la parte 5 donde se hizo creación de librerias e importación de ellas

---
+++ Libraries/custom_functions/temperature_methods.py
@@ -0,0 +1,64 @@
+#Se utilizan las funciones de la vez pasada y
+#se modifican para que tiren solamente resultados
+def recolectar_datos(x):
+    #Se crea el contador
+    i=0
+    for i in range(0,12):    
+        #Retomamos lo del primer ejercicio        
+        a=i+1
+        b=int(input("Por favor ingrese la temperatura {}:".format(a)))
+        c=x.append(b)
+#Creamos la segunda función
+def promedio(x):
+    sumatoria=sum(x)
+    promedio=sumatoria/12
+    return float(promedio)
+#Creamos la tercer función
+def mayor(x):
+    a=max(x)
+    return float(a)
+#Creamos la cuarta función
+def mes(x):
+    a=max(x)
+    b=x.index(a)
+    if b == 0:
+        c="Enero"
+    else:
+        if b == 1:
+            c="Febrero"
+        else:
+            if b == 2:
+                c="Marzo"
+            else:
+                if b == 3:
+                    c="Abril"
+                else:
+                    if b == 4:
+                        c="Mayo"
+                    else:
+                        if b == 5:
+                            c="Junio"
+                        else:
+                            if b == 6:
+                                c="Julio"
+                            else:
+                                if b == 7:
+                                    c="Agosto"
+                                else:
+                                    if b == 8:
+                                        c="Septiembre"
+                                    else:
+                                        if b == 9:
+                                            c="Octubre"
+                                        else:
+                                            if b == 10:
+                                                c="Noviembre"
+                                            else:
+                                                if b == 11:
+                                                    c="Diciembre"
+    #Se imprime los resultados
+    print(c)
+def des_estandar(x):
+    import statistics
+    a=statistics.stdev(x)
+    return float(a)
--- Libraries/main/laboratory.py
+++ Libraries/main/laboratory.py
@@ -1,70 +1,7 @@
 """
 Solución del laboratorio
 """
-#Se utilizan las funciones de la vez pasada y
-#se modifican para que tiren solamente resultados
-def recolectar_datos(x):
-    #Se crea el contador
-    i=0
-    for i in range(0,12):    
-        #Retomamos lo del primer ejercicio        
-        a=i+1
-        b=int(input("Por favor ingrese la temperatura {}:".format(a)))
-        c=x.append(b)
-#Creamos la segunda función
-def promedio(x):
-    sumatoria=sum(x)
-    promedio=sumatoria/12
-    print(promedio)
-#Creamos la tercer función
-def mayor(x):
-    a=max(x)
-    print(a)
-#Creamos la cuarta función
-def mes(x):
-    a=max(x)
-    b=x.index(a)
-    if b == 0:
-        c="Enero"
-    else:
-        if b == 1:
-            c="Febrero"
-        else:
-            if b == 2:
-                c="Marzo"
-            else:
-                if b == 3:
-                    c="Abril"
-                else:
-                    if b == 4:
-                        c="Mayo"
-                    else:
-                        if b == 5:
-                            c="Junio"
-                        else:
-                            if b == 6:
-                                c="Julio"
-                            else:
-                                if b == 7:
-                                    c="Agosto"
-                                else:
-                                    if b == 8:
-                                        c="Septiembre"
-                                    else:
-                                        if b == 9:
-                                            c="Octubre"
-                                        else:
-                                            if b == 10:
-                                                c="Noviembre"
-                                            else:
-                                                if b == 11:
-                                                    c="Diciembre"
-    #Se imprime los resultados
-    print(c)
-def des_estandar(x):
-    import statistics
-    a=statistics.stdev(x)
-    print(a)
+from custom_functions import temperature_methods
 #Creamos las listas necesarias
 santander=[]
 guajira=[]
@@ -78,76 +15,113 @@ a=int(input("Escoge las siguientes opciones: \n 1:Departamento de Santander \n 2
 if a==1:
     #Simplemente se recogen las funciones anteriores y se utilizan en el proyecto
     print("Departamento de Santander")
-    recolectar_datos(santander)
+    temperature_methods.recolectar_datos(santander)
     print("El promedio de las temperaturas en Santander fue de:")
-    promedio(santander)
+    print (temperature_methods.promedio(santander))
     print("La temperatura mayor en Santander fue de: ")
-    mayor(santander)
+    print (temperature_methods.mayor(santander))
     print("La temperatura donde fue mayor es en: ")
-    mes(santander)
+    temperature_methods.mes(santander)
     print("La desviación estándar es: ")
-    des_estandar(santander)
+    print (temperature_methods.des_estandar(santander))
     print("Departamento de Santander")
 else:
     if a==2:
         print("Departamento de La Guajira")
-        recolectar_datos(guajira)
+        temperature_methods.recolectar_datos(guajira)
         print("El promedio de las temperaturas en La Guajira fue de: ")
-        promedio(guajira)
+        print(temperature_methods.promedio(guajira))
         print("La temperatura mayor en La Guajira fue de: ")
-        mayor(guajira)
+        print (temperature_methods.mayor(guajira))
         print("El mes donde fue mayor es: ")
-        mes(guajira)
+        temperature_methods.mes(guajira)
         print("La desviación estándar es: ")
-        des_estandar(guajira)
+        print(temperature_methods.des_estandar(guajira))
         print("Departamento de La Guajira")
     else:
         if a == 3:
             print("Departamento de Nariño")
             recolectar_datos(narino)
             print("El promedio de las temperaturas en Nariño fue de: ")
-            promedio(narino)
+            print(promedio(narino))
             print("La temperatura mayor en Nariño fue de: ")
-            mayor(narino)
+            print(mayor(narino))
             print("El mes donde fue mayor es: ")
             mes(narino)
             print("La desviación estándar es de: ")
-            des_estandar(narino)
+            print(des_estandar(narino))
             print("Departamento de Nariño")
         else:
             if a == 4:
                 print("Departamento de Santander")
                 recolectar_datos(santander)
                 print("El promedio de las temperaturas en Santander fue de:")
-                promedio(santander)
+                print(promedio(santander))
                 print("La temperatura mayor en Santander fue de: ")
-                mayor(santander)
+                print(mayor(santander))
                 print("La temperatura donde fue mayor es en: ")
                 mes(santander)
                 print("La desviacón estándar es: ")
-                des_estandar(santander)
+                print(des_estandar(santander))
                 print("Departamento de Santander")
                 print("Departamento de La Guajira")
                 recolectar_datos(guajira)
                 print("El promedio de las temperaturas en La Guajira fue de:")
-                promedio(guajira)
+                print(promedio(guajira))
                 print("La temperatura mayor en La Guajira fue de: ")
-                mayor(guajira)
+                print(mayor(guajira))
                 print("La temperatura donde fue mayor es en: ")
                 mes(guajira)
                 print("La desviación estándar es: ")
-                des_estandar(guajira)
+                print(des_estandar(guajira))
                 print("Departamento de La Guajira")
                 print("Departamento de Nariño")
                 recolectar_datos(narino)
                 print("El promedio de las temperaturas en Nariño fue de:")
-                promedio(narino)
+                print(promedio(narino))
                 print("La temperatura mayor en Nariño fue de: ")
-                mayor(narino)
+                print (mayor(narino))
                 print("La temperatura donde fue mayor es en: ")
                 mes(narino)
                 print("La desviación estándar es: ")
-                des_estandar(narino)
+                print(des_estandar(narino))
                 print("Departamento de Narino")
-
+                prom_pais=((mayor(santander)+mayor(guajira)+mayor(guajira))/3)
+                print("El promedio del país fue de: ")
+                print(prom_pais)
+                if (mayor(santander)>mayor(narino)>mayor(guajira)):
+                    print("La temperatura mayor fue en Santander con: ")
+                    print(mayor(santander))
+                    print("En el mes de: ")
+                    mes(santander)
+                else:
+                    if(mayor(santander)>mayor(guajira)>mayor(narino)):
+                        print("La temperatura mayor fue en Santander con: ")
+                        print(mayor(santander))
+                        print("En el mes de: ")
+                        mes(santander)
+                    else:
+                        if(mayor(guajira)>mayor(santander)>mayor(narino)):
+                            print("La temperatura mayor fue en La Guajira con: ")
+                            print(mayor(guajira))
+                            print("En el mes de: ")
+                            mes(guajira)
+                        else:
+                            if (mayor(guajira)>mayor(narino)>mayor(santander)):
+                                print("La temperatura mayor fue en La Guajira con : ")
+                                print(mayor(guajira))
+                                print("En el mes de: ")
+                                mes(guajira)
+                            else:
+                                if (mayor(narino)>mayor(santander)>mayor(guajira)):
+                                    print("La temperatura mayor fue en Nariño con: ")
+                                    print(mayor(narino))
+                                    print("En el mes de: ")
+                                    mes(narino)
+                                else:
+                                    if (mayor(narino)>mayor(guajira)>mayor(santander)):
+                                        print("La temperatura mayor fue en Nariño con: ")
+                                        print(mayor(narino))
+                                        print("En el mes de: ")
+                                        mes(narino)
 #Desarrollado por Pedro Gómez / ID:000396221 / CACE Producciones      
